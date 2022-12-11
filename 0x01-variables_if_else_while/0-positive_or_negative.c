d3a4606a4acc32c383778c1f09445733ac57fe55
Tshepouser <tsheposekgobela04@gmail.com>
2022-12-11 11:42:01 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /**
  * main - Entry point
- * Description - Prints if random generated number is +tive, zero or -tive
+ * Description -Prints if random generated number is +tive, zero or -tive
  * Return: Always 0 (Success)
 */
 int main(void)
@@ -13,7 +13,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n= rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n == 0)
